62542d883f0731dceb2d509387ece070b4e62412
onlymi <49624485+onlymi@users.noreply.github.com>
2026-07-02 12:59:39 +0900

    feat: initial commit

---
+++ src/main/java/com/diarychat/DiaryChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.diarychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryChatApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryChatApiApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/diarychat/DiaryChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryChatApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
